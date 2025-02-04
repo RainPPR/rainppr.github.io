80cbd11941eabae5765338b99fefb67d94cf8b2a
RainPPR <41764766+RainPPR@users.noreply.github.com>
2025-02-04 13:20:09 +0800

    Update custom.js

--- docs/script/custom.js
+++ docs/script/custom.js
@@ -1,8 +1,19 @@
 // 示例：调用接口
-updateHeaderTitle("RainPPR's 个人介绍");
+updateHeaderTitle("RainPPR");
 setFooterContent("版权所有 &copy; 2025", "所有内容保留版权");
 setContentWidth(0.6);
-addMainContent("自我介绍", "<p>我是一名高中生。</p>");
-addSecondaryContent("技能", "<p>啥也没有。</p>");
-addMainContent("自我介绍", "<p>我是一名高中生。</p>");
-addSecondaryContent("技能", "<p>啥也没有。</p>");
+
+addMainContent("自我介绍", `<p>我是一名高中生。</p>`);
+
+addMainContent("我的博客", `<ul>
+<li>信息学博客：<a href="https://raineblog.github.io/blog/" class="uri">https://raineblog.github.io/blog/</a>。</li>
+<li>文化课博客：<a href="https://raineblog.github.io/whk/" class="uri">https://raineblog.github.io/whk/</a>。</li>
+</ul>`);
+
+addSecondaryContent("学业", `<ul>
+<li>山东省青岛第二中学。</li>
+<li>2027 高考。</li>
+</ul>`);
+
+addSecondaryContent("技能", `<p>awa</p>
+<p>使用 <a href="https://pandoc.org/try/" class="uri">https://pandoc.org/try/</a> 生成。</p>`);
