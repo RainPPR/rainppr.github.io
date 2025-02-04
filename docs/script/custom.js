af4045dade6fb482ab52c7a9e5162797e7903f75
RainPPR <41764766+RainPPR@users.noreply.github.com>
2025-02-04 15:58:59 +0800

    Update style.css

--- docs/script/custom.js
+++ docs/script/custom.js
@@ -5,15 +5,24 @@ setContentWidth(0.6);
 
 addMainContent("自我介绍", `<p>我是一名高中生。</p>`);
 
-addMainContent("我的博客", `<ul>
+addMainContent(
+    "我的博客",
+    `<ul>
 <li>信息学博客：<a href="https://raineblog.github.io/blog/" class="uri">https://raineblog.github.io/blog/</a>。</li>
 <li>文化课博客：<a href="https://raineblog.github.io/whk/" class="uri">https://raineblog.github.io/whk/</a>。</li>
-</ul>`);
+</ul>`
+);
 
-addSecondaryContent("学业", `<ul>
+addSecondaryContent(
+    "学业",
+    `<ul>
 <li>山东省青岛第二中学。</li>
 <li>2027 高考。</li>
-</ul>`);
+</ul>`
+);
 
-addSecondaryContent("技能", `<p>awa</p>
-<p>使用 <a href="https://pandoc.org/try/" class="uri">https://pandoc.org/try/</a> 生成。</p>`);
+addSecondaryContent(
+    "技能",
+    `<p>awa</p>
+<p>使用 <a href="https://pandoc.org/try/" class="uri">https://pandoc.org/try/</a> 生成。</p>`
+);
