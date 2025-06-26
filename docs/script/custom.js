41d97b602998c34152e6fbbd40e138bd1b912116
RainPPR <41764766+RainPPR@users.noreply.github.com>
2025-06-26 17:15:35 +0800

    Update custom.js

--- docs/script/custom.js
+++ docs/script/custom.js
@@ -1,7 +1,7 @@
 // 示例：调用接口
-updateHeaderTitle("RainPPR");
-setFooterContent("版权所有 &copy; 2025", "所有内容保留版权");
-setContentWidth(0.6);
+updateHeaderTitle("About RainPPR");
+setFooterContent("by RainPPR", "2025-06-26");
+setContentWidth(0.5);
 
 addMainContent("自我介绍", `<p>我是一名高中生。</p>`);
 
@@ -16,13 +16,13 @@ addMainContent(
 addSecondaryContent(
     "学业",
     `<ul>
-<li>山东省青岛第二中学。</li>
+<li>2024 中考（已上岸）。</li>
 <li>2027 高考。</li>
 </ul>`
 );
 
 addSecondaryContent(
     "技能",
-    `<p>awa</p>
-<p>使用 <a href="https://pandoc.org/try/" class="uri">https://pandoc.org/try/</a> 生成。</p>`
+    `<p>我是辣鸡。</p>
+<p>使用 <a href="https://pandoc.org/try/" class="uri">https://pandoc.org/try/</a> 生成 HTML。</p>`
 );
